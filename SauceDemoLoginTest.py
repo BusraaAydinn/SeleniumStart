d34aa3b179bd9d1c6adbc3196738835b0b9a04f6
BusraaAydinn <busraaaydinn67@gmail.com>
2023-12-25 01:11:51 +0300

    Second Commit

--- SauceDemoLoginTest.py
+++ SauceDemoLoginTest.py
@@ -94,11 +94,160 @@ class Sauce_Demo_Login_Test:
             print("TEST SONUCU: True")
         else:
             print("TEST SONUCU: False")
-    
-         
 
-testClass = Sauce_Demo_Login_Test()
-testClass.test_passing_empty()
-testClass.test_invalid_password()
-testClass.test_invalid_login()
-testClass.test_product_control()
+#sitedeki seçtiğin ilk ürüne tıkladığında detay sayfasına gidiliyor mu?         
+class Sauce_Demo_Product_Test:
+    def test_product_details(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() 
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        current_url = driver.current_url 
+        expected_url = "https://www.saucedemo.com/inventory.html"
+        if (current_url == expected_url):
+            first_test = True
+        else:
+            first_test = False
+        first_product = driver.find_element(By.ID,"item_4_title_link")
+        first_product.click()
+        sleep(2)
+        current_url = "https://www.saucedemo.com/inventory.html"
+        expected_url = "https://www.saucedemo.com/inventory-item.html?id=4"
+        if (current_url == expected_url):
+            first_test = True
+        else:
+            first_test = False
+
+#Sayfada iki adet ürün seçildiğinde sepet ikonu üzerindeki 2 sayısıyla seçilen ürün adeti sayısı eşit mi?
+
+    def test_product_quantity(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() 
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        current_url = driver.current_url 
+        expected_url = "https://www.saucedemo.com/inventory.html"
+        if (current_url == expected_url):
+            first_test = True
+        else:
+            first_test = False
+        addToCart1 = driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack")
+        addToCart1.click()
+        sleep(1)
+        addToCart2 = driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light")
+        addToCart2.click()
+        sleep(1)
+        addToCart3 = driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt")
+        addToCart3.click()
+        sleep(1)
+        products = driver.find_elements(By.CLASS_NAME,"btn btn_secondary btn_small btn_inventory")
+        first_test = len(products) == 3
+        cart_badge = driver.find_elements(By.CLASS_NAME,"shopping_cart_badge")
+        second_test = len(cart_badge) == 3
+        if (first_test == second_test):
+            print("TEST SONUCU: True")
+        else:
+            print("TEST SONUCU: False")
+
+#Siteden hatasız bir şekilde çıkış yapılıyor mu?     
+
+class Sauce_Demo_Logout_Test:              
+    def test_logout(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() 
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        current_url = driver.current_url 
+        expected_url = "https://www.saucedemo.com/inventory.html"
+        if (current_url == expected_url):
+            first_test = True
+        else:
+            first_test = False
+        sleep(2) 
+        menuButton = driver.find_element(By.ID,"react-burger-menu-btn")
+        menuButton.click()
+        sleep(2)
+        logoutButton = driver.find_element(By.ID,"logout_sidebar_link")
+        logoutButton.click()
+        sleep(2)
+        driver.get("https://www.saucedemo.com")
+        
+#Alfabetik sıraya göre filtreleme yapılıyor mu?
+        
+class Sauce_Demo_Filter_Test:
+    def test_filter(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() 
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        current_url = driver.current_url 
+        expected_url = "https://www.saucedemo.com/inventory.html"
+        if (current_url == expected_url):
+            first_test = True
+        else:
+            first_test = False
+        sleep(2) 
+        filterButton = driver.find_element(By.CLASS_NAME,"product_sort_container")
+        filterButton.click()
+        sleep(2) 
+        zToa = driver.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div[2]/div/span/select/option[2]")
+        zToa.click()
+        sleep(2)
+        first_product = driver.find_element(By.ID,"item_4_title_link")
+        second_product = driver.find_element(By.ID,"item_0_title_link")
+        third_product = driver.find_element(By.ID,"item_1_title_link")
+        forth_product = driver.find_element(By.ID,"item_5_title_link")
+        fifth_product = driver.find_element(By.ID,"item_2_title_link")
+        sixth_product = driver.find_element(By.ID,"item_3_title_link")
+        list = ['first_product', 'second_product', 'third_product', 'forth__product', 'fifth_product', 'sixth_product']
+        reverse_list = sorted(list, reverse=True)
+        print(reverse_list)
+    
+# testClass = Sauce_Demo_Login_Test()
+# testClass.test_passing_empty()
+# testClass.test_invalid_password()
+# testClass.test_invalid_login()
+# testClass.test_product_control()
+# testClass = Sauce_Demo_Product_Test()
+# testClass.test_product_details()
+# testClass.test_product_quantity()
+# testClass = Sauce_Demo_Logout_Test()
+# testClass.test_logout()
+testClass = Sauce_Demo_Filter_Test()
+testClass.test_filter()
--- Test_DemoClass.py
+++ Test_DemoClass.py
@@ -4,52 +4,39 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
 from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
-from selenium.webdriver.common.action_chains import ActionChains 
+import pytest
 
-class Test_SauceDemo:
-    def __init__(self):
+class Test_DemoClass:
+    #prefix => test_ 
+    def setup_method(self): #her test başlangıcında çalışacak fonk
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.saucedemo.com")
-        self.driver.maximize_window() #ekranı büyütür
+        self.driver.maximize_window() 
 
-    def test_invalid_login(self):
+    def teardown_method(self): # her testinin bitiminde çalışacak fonk
+        self.driver.quit()
+
+    @pytest.mark.parametrize("username,password",[("1","secret_sauce"),("problem_user","1")])
+    def test_invalid_login(self,username,password):
         usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-        #usernameInput = driver.find_element(By.ID,"user-name")
-        usernameInput.send_keys("locked_out_user")
+        usernameInput.send_keys(username)
         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
-        #passwordInput = driver.find_element(By.ID,"password")
-        passwordInput.send_keys("secret_sauce")
+        passwordInput.send_keys(password)
         loginButton = self.driver.find_element(By.ID,"login-button")
         loginButton.click()
         errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
-        testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
-        print(f"TEST SONUCU: {testResult}")
+        assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
 
-    def valid_login(self):
-        self.driver.get("https://www.saucedemo.com")
+    @pytest.mark.skip
+    def test_valid_login(self):
         usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
         usernameInput.send_keys("standard_user")
         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
         passwordInput.send_keys("secret_sauce")
         loginButton = self.driver.find_element(By.ID,"login-button")
         loginButton.click()
-        #anasayfaya ulaşıp ulaşamadığımın kanıtı
-        #headerLogo = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='header_container']/div[1]/div[2]/div")))
-        #testResult = headerLogo.text == "Swag Labs"
         self.driver.execute_script("window.scrollTo(0,500)") #penceremin scrolunu aşağıya indiriyor,verdiğim koordinatlarla birlikte
         addToCart = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")))
         addToCart.click()
         remove = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='remove-test.allthethings()-t-shirt-(red)']")))
-        testResult = remove.text == "Remove"
-        print(f"TEST SONUCU: {testResult}")
-        sleep(5)
-        
-        """ addToCart = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")))
-        actionChains = ActionChains(self.driver)
-        actionChains.move_to_element(addToCart) #butonun olduğu yere sayfayı taşı
-        actionChains.click()
-        actionChains.perform() #depolanan tüm fonksiyonları çalıştırır """
-
-testClass = Test_SauceDemo()
-testClass.test_invalid_login()
-testClass.valid_login()
+        assert remove.text == "Remove"
